d15d4a5b9bb9ae78cef366ae549e76f295c76367
Jorn Eilander <jorn.eilander@azorion.com>
2019-12-20 17:40:53 +0100

    Day 7 - Part 2

--- 2019/Day 7 - Amplification Circuit/solution.py
+++ 2019/Day 7 - Amplification Circuit/solution.py
@@ -4,110 +4,119 @@ from itertools import permutations
 chdir("./2019/Day 7 - Amplification Circuit/")
 
 
-def process_intcode(intcode, input_param_one=None, input_param_two=None):
-    mutable_intcode = list(map(lambda x: int(x), intcode.split(","))).copy()
+class Amplifier:
+    def __init__(self, intcode: str, phase_setting: int, input: int = None):
+        self.intcode: list[int] = list(map(lambda x: int(x), intcode.split(",")))
+        self.phase_setting: int = phase_setting
+        self.input: list[int] = [self.phase_setting] if input is None else [self.phase_setting, input]
+        self.instruction_pointer: int = 0
+        self.input_pointer: int = 0
+        self.output: int = 0
 
-    def inner(inner_intcode, instruction_pointer=0):
-        instruction = f"{inner_intcode.copy()[instruction_pointer]:05d}"
+    def get_next_output(self):
+        instruction = f"{self.intcode[self.instruction_pointer]:05d}"
 
         print(f"Instruction: {instruction}")
 
         opcode = int(instruction[3:])
         first_param_mode = int(instruction[2:3])
         second_param_mode = int(instruction[1:2])
-        third_param_mode = int(instruction[0:1])
+        third_param_mode = int(instruction[0:1])  # noqa F841
 
-        try:
-            if opcode == 99:
-                return inner.return_output
+        if opcode == 99:
+            print(f"Opcode: {opcode}")
+            raise Exception("Reached end of intcode")
 
-            if opcode == 1:
-                command = inner_intcode[instruction_pointer:instruction_pointer + 4]
+        if opcode == 1:
+            command = self.intcode[self.instruction_pointer:self.instruction_pointer + 4]
 
-                param_one_value = inner_intcode[command[1]] if first_param_mode == 0 else command[1]
-                param_two_value = inner_intcode[command[2]] if second_param_mode == 0 else command[2]
-                print(f"Opcode: {opcode}, command: {command}, Param1: {param_one_value}, Param2: {param_two_value}")
-                inner_intcode[command[3]] = param_one_value + param_two_value
-                return inner(inner_intcode, instruction_pointer + 4)
+            param_one_value = self.intcode[command[1]] if first_param_mode == 0 else command[1]
+            param_two_value = self.intcode[command[2]] if second_param_mode == 0 else command[2]
+            # print(f"Opcode: {opcode}, command: {command}, Param1: {param_one_value}, Param2: {param_two_value}")
+            self.intcode[command[3]] = param_one_value + param_two_value
+            self.instruction_pointer = self.instruction_pointer + 4
+            return self.get_next_output()
 
-            if opcode == 2:
-                command = inner_intcode[instruction_pointer:instruction_pointer + 4]
-                param_one_value = inner_intcode[command[1]] if first_param_mode == 0 else command[1]
-                param_two_value = inner_intcode[command[2]] if second_param_mode == 0 else command[2]
-                print(f"Opcode: {opcode}, command: {command}, Param1: {param_one_value}, Param2: {param_two_value}")
-                inner_intcode[command[3]] = param_one_value * param_two_value
-                return inner(inner_intcode, instruction_pointer+4)
+        if opcode == 2:
+            command = self.intcode[self.instruction_pointer:self.instruction_pointer + 4]
+            param_one_value = self.intcode[command[1]] if first_param_mode == 0 else command[1]
+            param_two_value = self.intcode[command[2]] if second_param_mode == 0 else command[2]
+            print(f"Opcode: {opcode}, command: {command}, Param1: {param_one_value}, Param2: {param_two_value}")
+            self.intcode[command[3]] = param_one_value * param_two_value
+            self.instruction_pointer = self.instruction_pointer + 4
+            return self.get_next_output()
 
-            if opcode == 3:
-                command = inner_intcode[instruction_pointer:instruction_pointer + 2]
-                print(f"Opcode: {opcode}, command: {command}")
-                inner_intcode[command[1]] = input_param_one if inner.input_param_pos == 0 else input_param_two
-                inner.input_param_pos += 1
-                return inner(inner_intcode, instruction_pointer+2)
+        if opcode == 3:
+            command = self.intcode[self.instruction_pointer:self.instruction_pointer + 2]
+            print(f"Opcode: {opcode}, command: {command}")
+            self.intcode[command[1]] = self.input[self.input_pointer]
+            self.input_pointer = self.input_pointer + 1
+            self.instruction_pointer = self.instruction_pointer + 2
+            return self.get_next_output()
 
-            if opcode == 4:
-                command = inner_intcode[instruction_pointer:instruction_pointer + 2]
-                param_one_value = inner_intcode[command[1]] if first_param_mode == 0 else command[1]
-                print(f"Opcode: {opcode}, command: {command}, Param1: {param_one_value}")
-                inner.return_output = param_one_value
-                return inner(inner_intcode, instruction_pointer+2)
+        if opcode == 4:
+            command = self.intcode[self.instruction_pointer:self.instruction_pointer + 2]
+            param_one_value = self.intcode[command[1]] if first_param_mode == 0 else command[1]
+            print(f"Opcode: {opcode}, command: {command}, Param1: {param_one_value}")
 
-            if opcode == 5:
-                command = inner_intcode[instruction_pointer:instruction_pointer + 3]
-                param_one_value = inner_intcode[command[1]] if first_param_mode == 0 else command[1]
-                param_two_value = inner_intcode[command[2]] if second_param_mode == 0 else command[2]
-                print(f"Opcode: {opcode}, command: {command}, Param1: {param_one_value}, Param2: {param_two_value}")
+            self.instruction_pointer = self.instruction_pointer + 2
+            return param_one_value
 
-                if param_one_value != 0:
-                    return inner(inner_intcode, param_two_value)
-                return inner(inner_intcode, instruction_pointer+3)
+        if opcode == 5:
+            command = self.intcode[self.instruction_pointer:self.instruction_pointer + 3]
+            param_one_value = self.intcode[command[1]] if first_param_mode == 0 else command[1]
+            param_two_value = self.intcode[command[2]] if second_param_mode == 0 else command[2]
+            print(f"Opcode: {opcode}, command: {command}, Param1: {param_one_value}, Param2: {param_two_value}")
+            self.instruction_pointer = param_two_value if param_one_value != 0 else self.instruction_pointer + 3
+            return self.get_next_output()
 
-            if opcode == 6:
-                command = inner_intcode[instruction_pointer:instruction_pointer + 3]
-                param_one_value = inner_intcode[command[1]] if first_param_mode == 0 else command[1]
-                param_two_value = inner_intcode[command[2]] if second_param_mode == 0 else command[2]
-                print(f"Opcode: {opcode}, command: {command}, Param1: {param_one_value}, Param2: {param_two_value}")
+        if opcode == 6:
+            command = self.intcode[self.instruction_pointer:self.instruction_pointer + 3]
+            param_one_value = self.intcode[command[1]] if first_param_mode == 0 else command[1]
+            param_two_value = self.intcode[command[2]] if second_param_mode == 0 else command[2]
+            print(f"Opcode: {opcode}, command: {command}, Param1: {param_one_value}, Param2: {param_two_value}")
+            self.instruction_pointer = param_two_value if param_one_value == 0 else self.instruction_pointer + 3
+            return self.get_next_output()
 
-                if param_one_value == 0:
-                    return inner(inner_intcode, param_two_value)
-                return inner(inner_intcode, instruction_pointer+3)
+        if opcode == 7:
+            command = self.intcode[self.instruction_pointer:self.instruction_pointer + 4]
+            param_one_value = self.intcode[command[1]] if first_param_mode == 0 else command[1]
+            param_two_value = self.intcode[command[2]] if second_param_mode == 0 else command[2]
+            print(f"Opcode: {opcode}, command: {command}, Param1: {param_one_value}, Param2: {param_two_value}")
+            self.intcode[command[3]] = 1 if param_one_value < param_two_value else 0
+            self.instruction_pointer = self.instruction_pointer + 4
+            return self.get_next_output()
 
-            if opcode == 7:
-                command = inner_intcode[instruction_pointer:instruction_pointer + 4]
-                param_one_value = inner_intcode[command[1]] if first_param_mode == 0 else command[1]
-                param_two_value = inner_intcode[command[2]] if second_param_mode == 0 else command[2]
-                print(f"Opcode: {opcode}, command: {command}, Param1: {param_one_value}, Param2: {param_two_value}")
-                inner_intcode[command[3]] = 1 if param_one_value < param_two_value else 0
-                return inner(inner_intcode, instruction_pointer+4)
+        if opcode == 8:
+            command = self.intcode[self.instruction_pointer:self.instruction_pointer + 4]
+            param_one_value = self.intcode[command[1]] if first_param_mode == 0 else command[1]
+            param_two_value = self.intcode[command[2]] if second_param_mode == 0 else command[2]
 
-            if opcode == 8:
-                command = inner_intcode[instruction_pointer:instruction_pointer + 4]
-                param_one_value = inner_intcode[command[1]] if first_param_mode == 0 else command[1]
-                param_two_value = inner_intcode[command[2]] if second_param_mode == 0 else command[2]
-
-                inner_intcode[command[3]] = 1 if param_one_value == param_two_value else 0
-                return inner(inner_intcode, instruction_pointer+4)
-        except:
-            return None
-
-    inner.return_output = 0
-    inner.input_param_pos = 0
-    return inner(mutable_intcode)
+            self.intcode[command[3]] = 1 if param_one_value == param_two_value else 0
+            self.instruction_pointer = self.instruction_pointer + 4
+            return self.get_next_output()
 
 
 def combined_thruster_output(intcode: str, phase_settings: tuple):
-    output = 0
-    for phase_setting in phase_settings:
-        output = process_intcode(intcode, input_param_one=phase_setting, input_param_two=output)
-    return int(output)
+    print(f"Trying: {phase_settings}")
+    amplifiers: list[Amplifier] = [Amplifier(intcode, phase_setting) for phase_setting in phase_settings]
+    amplifiers_pointer: int = 0
+    output: int = 0
 
+    try:
+        while True:
+            amplifiers[amplifiers_pointer].input.append(output)
+            output = amplifiers[amplifiers_pointer].get_next_output()
+            amplifiers_pointer = 0 if amplifiers_pointer == len(phase_settings) - 1 else amplifiers_pointer + 1
+    except Exception:
+        print(f"Output: {output}")
+        return output
 
 
 with open("./input.txt", "r") as file:
     input = file.readline()
 
-# input = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
+all_combinations = permutations(range(5, 10))
 
-all_combinations = permutations(range(5))
-
-print(max((list((combined_thruster_output(input, combination)) for combination in all_combinations))))
+output = list((combined_thruster_output(input, combination)) for combination in all_combinations)
+print(max(output))
