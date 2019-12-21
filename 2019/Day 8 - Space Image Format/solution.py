caeab2e5879e4c735bb7a72715cd36faf0d0eefe
Jorn Eilander <jorn.eilander@azorion.com>
2019-12-21 20:45:41 +0100

    Day 8 - Part 2

--- 2019/Day 8 - Space Image Format/solution.py
+++ 2019/Day 8 - Space Image Format/solution.py
@@ -1,22 +1,24 @@
 from os import chdir
-import operator
 
 chdir("./2019/Day 8 - Space Image Format/")
 
 
 def chunks(text: str, size: int):
-    text_pos = 0
+    text_pos: int = 0
     while text_pos < len(text) - 1:
-        yield text[text_pos:text_pos + size - 1]
+        yield text[text_pos:text_pos + size]
         text_pos = text_pos + size
 
 
 class Layer():
     def __init__(self, width: int, height: int, layer_code: str):
         self.layer_code: str = layer_code
-        self.rows: list = [x for x in chunks(layer_code, width)]
+        self.rows: list = [row for row in chunks(layer_code, width)]
         self.height: int = height
         self.width: int = width
+
+    def __repr__(self):
+        return self.layer_code
 
 
 class Image():
@@ -29,17 +31,27 @@ class Image():
             Layer(self.width, self.height, layer_code)
             for layer_code in chunks(spi_encoded_image, self.layer_size)
         ]
+        self.final_image = Layer(self.width, self.height, "".join(self._render_image()))
+
+    def _render_image(self):
+        reversed_layers = reversed(self.layers)
+        image_pixels: list = ["_" for x in range(self.layer_size)]
+
+        for layer in reversed_layers:
+            for x in range(self.layer_size):
+                if layer.layer_code[x] == "0":
+                    image_pixels[x] = "_"
+                elif layer.layer_code[x] == "1":
+                    image_pixels[x] = "8"
+                elif layer.layer_code[x] == "2":
+                    pass
+        return image_pixels
 
 
 with open("./input.txt", "r") as file:
     input = file.readline()
 
-print(f"Lenght of input: {len(input)}")
 image = Image(25, 6, input)
 
-layers_with_0_count = [(layer.layer_code.count("0"), image.layers.index(layer)) for layer in image.layers]
-layer_with_least_0_count = image.layers[(min(layers_with_0_count, key=operator.itemgetter(0))[1])]
-ones = layer_with_least_0_count.layer_code.count("1")
-twos = layer_with_least_0_count.layer_code.count("2")
-
-print(ones * twos)
+for row in image.final_image.rows:
+    print("".join(row))
